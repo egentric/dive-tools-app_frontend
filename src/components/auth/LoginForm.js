fbeedd3230ce2ab9bbea925e1d6dbafb15b0722d
myrdinaptaliesen <nicog.dev@gmail.com>
2023-07-27 13:59:08 +0200

    Fix méga bug page Login

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -11,7 +11,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const validationError = useState({});
+  const [validationError] = useState({});
   const [errorMessage, setErrorMessage] = useState("");
   const setAuth = useState({
     isLoggedIn: false,
