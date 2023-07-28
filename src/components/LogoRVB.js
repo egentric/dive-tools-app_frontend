596142f9713af5501130dc26d9a01ac0b8da4fb0
egentric <85742839+egentric@users.noreply.github.com>
2023-07-28 11:20:57 +0200

    Update LogoRVB.js

--- src/components/LogoRVB.js
+++ src/components/LogoRVB.js
@@ -2,7 +2,11 @@ import React from "react";
 const LogoRVB = () => {
   return (
     <div className="logo2">
-      <img src="../../img/logoGaspar130.png" alt="logo Gaspar" width={120} />
+      <img
+        src={process.env.PUBLIC_URL + "/img/logoGaspar130.png"}
+        alt="logo Gaspar"
+        width={120}
+      />
     </div>
   );
 };
