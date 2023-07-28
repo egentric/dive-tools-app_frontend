10248cbfb61e7d04612849fffc236ba2b3541f7b
egentric <85742839+egentric@users.noreply.github.com>
2023-07-28 10:12:24 +0200

    Update LoginForm.js

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -30,7 +30,7 @@ const Login = () => {
     // }
 
     axios
-      .post(`http://api.erwangentric.fr/api/login`, formData)
+      .post(`https://api.erwangentric.fr/api/login`, formData)
       .then((response) => {
         if (response.data.status === "success") {
           localStorage.setItem(
